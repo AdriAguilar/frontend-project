b9a155dc4cf71df15d49cf868374c1c54b68a0ec
Adrian Aguilar Triviño <adritrivi41@gmail.com>
2023-03-01 20:04:49 +0100

    form create product and categories db request

--- src/app/products/pages/product-create/product-create.component.ts
+++ src/app/products/pages/product-create/product-create.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../../interfaces/Categories.interface';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.scss']
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
+  categories$: Observable<Category[]>;
 
+  constructor( private cs: CategoriesService ) { }
+
+  ngOnInit(): void {
+    this.categories$ = this.cs.getAllCategories();
+  }
 }
