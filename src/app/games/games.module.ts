6d65c5651e2633e71a6dedf7caa17b5522c87a5b
Pedro Gil <pgilperez.12.pgp@gmail.com>
2023-03-02 19:29:46 +0100

    va3

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GamesRoutingModule } from './games-routing.module';
+import { GamesListComponent } from './pages/games-list/games-list.component';
+import { GamesDetailComponent } from './pages/games-detail/games-detail.component';
 
 
 
 @NgModule({
   declarations: [
-    
+    GamesListComponent,
+    GamesDetailComponent,
   ],
   imports: [
     CommonModule,
