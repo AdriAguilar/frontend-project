5cae327c7580916fb9276eedd7dc25e1481229d7
AdriAguilar <106266324+AdriAguilar@users.noreply.github.com>
2023-06-08 19:45:33 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShoppingComponent } from './games/pages/shopping/shopping.component';
 import { MatIconModule } from '@angular/material/icon';
 
