4aae5c0c97ae8a740294bdca99894bd822e88a11
AdriAguilar <adritrivi41@gmail.com>
2023-06-13 19:56:30 +0200

    Show all users on users list chat

--- src/app/chats/components/user-list/user-list.component.ts
+++ src/app/chats/components/user-list/user-list.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 import { User } from 'src/app/interfaces/Response.interface';
 import { ChatService } from '../../services/chat.service';
+import { UsersService } from '../../data/users.service';
 
 @Component({
   selector: 'app-user-list',
@@ -12,12 +13,16 @@ export class UserListComponent {
   @Output() userSelected = new EventEmitter<User>();
   users: User[] = [];
 
-  constructor( private cs: ChatService ) { }
+  constructor( private cs: ChatService,
+               private us: UsersService ) { }
 
   ngOnInit(): void {
-    this.cs.userList$.subscribe( users => {
-      this.users = users;
-    });
+    this.us.init();
+    this.us.users$.subscribe( users => this.users = users );
+    
+    // this.cs.userList$.subscribe( users => {
+    //   this.users = users;
+    // });
   }
 
   sendUser( user: User ): void {
